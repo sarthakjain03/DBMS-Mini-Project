3cbe252c8ffafe8fe1fc08a7a494656403323459
Sarthak Jain <jainsarthak2003@gmail.com>
2023-04-14 20:49:28 +0530

    added, linked html files

--- homepage.js
+++ homepage.js
@@ -4,3 +4,15 @@ function staffListPage() {
 function staffRegPage() {
     window.location.href = "staff_reg.html"
 }
+function clientRegPage() {
+    window.location.href = "client_reg.html"
+}
+function propListPg() {
+    window.location.href = "prop_list.html"
+}
+function propReportPg() {
+    window.location.href = "prop_report.html"
+}
+function propRegPage() {
+    window.location.href = "prop_reg.html"
+}
