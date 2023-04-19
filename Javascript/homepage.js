e9cc9d86ab6939fdbc656c4f307faa922beda4bb
Vishal <Vishalarya2468@gmail.com>
2023-04-19 23:02:31 +0530

    StaffListing,Property Listing and Report Page Added

---
+++ Javascript/homepage.js
@@ -0,0 +1,24 @@
+function staffListPage() {
+    window.location.href = "staff_listing.html"
+}
+function staffRegPage() {
+    window.location.href = "staff_reg.html"
+}
+function clientRegPage() {
+    window.location.href = "client_reg.html"
+}
+function propListPg() {
+    window.location.href = "prop_list.html"
+}
+function propReportPg() {
+    window.location.href = "prop_report.html"
+}
+function propRegPage() {
+    window.location.href = "prop_reg.html"
+}
+function leaseFormPg() {
+    window.location.href = "lease_form.html"
+}
+function ownerRegPg() {
+    window.location.href = "owner_reg.html"
+}
